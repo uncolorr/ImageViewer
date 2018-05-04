332432273f035fc4c5230e1f6355e309a4df1646
uncolor <sapozhnikov.v@postuf.com>
2018-05-04 20:38:03 +0300

    Добавил передачу параметров во фрагмент с изображением, добавил проверку на null

--- app/src/main/java/com/colorblind/uncolor/imageviewer/image_slider_activity/ImagePagerAdapter.java
+++ app/src/main/java/com/colorblind/uncolor/imageviewer/image_slider_activity/ImagePagerAdapter.java
@@ -1,34 +1,44 @@
 package com.colorblind.uncolor.imageviewer.image_slider_activity;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import com.colorblind.uncolor.imageviewer.models.ImageItem;
+
+import java.util.ArrayList;
 
 /**
  * Created by Uncolor on 02.05.2018.
  */
 
-public class ImagePagerAdapter extends FragmentPagerAdapter {
+public class ImagePagerAdapter extends FragmentPagerAdapter{
 
-    private int count = 0;
+    private ArrayList<ImageItem> images;
 
-    public ImagePagerAdapter(FragmentManager fm, int count) {
+    public ImagePagerAdapter(FragmentManager fm, ArrayList<ImageItem> images) {
         super(fm);
-        this.count = count;
+        this.images = removeNullableItem(images);
+    }
+
+    private ArrayList<ImageItem> removeNullableItem(ArrayList<ImageItem> images){
+        images.remove(null);
+        return images;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return ImageFragment_.builder().build();
+        ImageFragment fragment = ImageFragment_.builder().build();
+        Bundle args = new Bundle();
+        args.putString(ImageFragment.ARG_IMAGE_URL, images.get(position).getPhoto604());
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return count;
+        return images.size();
     }
 
-
-    public void setCount(int count) {
-        this.count = count;
-    }
 }
